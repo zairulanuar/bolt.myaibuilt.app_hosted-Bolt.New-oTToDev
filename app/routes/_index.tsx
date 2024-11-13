cae262d1fd3b4e14be785f77ff1c89a240bb19e5
eduardruzga <wonderwhy.er@gmail.com>
2024-11-13 11:17:22 +0200

    Change header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { Chat } from '~/components/chat/Chat.client';
 import { Header } from '~/components/header/Header';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Free Bolt.MyAiBuilt.APP' }, { name: 'description', content: 'Free AI Web App Builder – No Setup Needed!' }];
 };
 
 export const loader = () => json({});
