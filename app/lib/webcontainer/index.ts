8d7a7f5bfc6468bead8471ce0e7d5e06194619a7
eduardruzga <wonderwhy.er@gmail.com>
2024-12-24 11:43:54 +0200

    Cleanup urls in stacktrace

--- app/lib/webcontainer/index.ts
+++ app/lib/webcontainer/index.ts
@@ -1,5 +1,6 @@
 import { WebContainer } from '@webcontainer/api';
 import { WORK_DIR_NAME } from '~/utils/constants';
+import { cleanStackTrace } from '~/utils/stacktrace';
 
 interface WebContainerContext {
   loaded: boolean;
@@ -43,7 +44,7 @@ if (!import.meta.env.SSR) {
               type: 'preview',
               title: isPromise ? 'Unhandled Promise Rejection' : 'Uncaught Exception',
               description: message.message,
-              content: `Error occurred at ${message.pathname}${message.search}${message.hash}\nPort: ${message.port}\n\nStack trace:\n${message.stack}`,
+              content: `Error occurred at ${message.pathname}${message.search}${message.hash}\nPort: ${message.port}\n\nStack trace:\n${cleanStackTrace(message.stack || '')}`,
               source: 'preview',
             });
           }
